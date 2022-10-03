c944009407dd036b9dac05c469f108e6f4cf73a7
mpxf <diamond061@outlook.com>
2022-10-03 09:18:42 -0400

    7zoip

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int randint = r.nextInt();
+
+        System.out.println("Your random number is: " + randint);
+
+        while (true){
+            ; // optimize gpu cpu :thumbsup:
+        }
+    }
+}
